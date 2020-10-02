6f5b3dd18fb1f189cf33b6c3259a54b3234e3b85
Jonathon Simonton <jsimonton020@gmail.com>
2020-10-01 18:58:17 -0700

    fixed duplicate security rule names

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -92,7 +92,7 @@ resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-instance"
 }
 
-resource "aws_security_group_rule" "allow_http_inbound" {
+resource "aws_security_group_rule" "allow_server_http_inbound" {
   type = "ingress"
   security_group_id = aws_security_group.instance.id
 
